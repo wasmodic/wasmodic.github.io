825feb8cc81746c85db70a8dd849f9ded02d22ea
lonsbio <andrew.lonsdale@lonsbio.com.au>
2025-11-22 22:52:12 +1100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -122,7 +122,7 @@ const examples = [
   <!-- Reference Links Grid -->
     <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6 mb-8">
         {#each examples as link}
-            <a href={link.link} target="_blank" rel="noopener noreferrer" class="card preset-tonal-primary hover:preset-filled-primray-50-950 transition-all duration-200 p-6 space-y-2 group">
+            <a href={link.link} target="_blank" rel="noopener noreferrer" class="card preset-tonal-primary hover:preset-filled-primray-950 transition-all duration-200 p-6 space-y-2 group">
                 <h3 class="h4 group-hover:underline">{link.name}</h3>
                 <p class="opacity-70">{link.description}</p>
             </a>
@@ -133,7 +133,7 @@ const examples = [
 
     <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6 mb-8">
         {#each references as link}
-            <a href={link.link} target="_blank" rel="noopener noreferrer" class="card preset-tonal-secondary-50-950 transition-all duration-200 p-6 space-y-2 group">
+            <a href={link.link} target="_blank" rel="noopener noreferrer" class="card preset-tonal-secondary-50 hover:preset-filled-primray-950 transition-all duration-200 p-6 space-y-2 group">
                 <h3 class="h4 group-hover:underline">{link.name}</h3>
                 <p class="opacity-70">{link.description}</p>
             </a>
